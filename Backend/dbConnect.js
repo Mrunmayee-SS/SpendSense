bfeda51cdd663644f9acc5f38266c95c37720249
Sharman <sharmanghube@gmail.com>
2024-11-12 02:11:22 +0530

    Updated

--- Backend/dbConnect.js
+++ Backend/dbConnect.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/expense_tracker')
+mongoose.connect(process.env.MONGODB_URI);
+
 
 const connection = mongoose.connection
 
 connection.on('error', err => console.log(err))
 
 connection.on('connected' , () => console.log('Mongo DB Connection Successfull'))
+
+
+
