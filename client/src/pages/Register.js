8fd95b34aa484024342fa16838dd0691f5382c8c
Sharman <sharmanghube@gmail.com>
2024-11-12 17:26:09 +0530

    updated links

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -12,7 +12,7 @@ function Register() {
   const onFinish = async (values) => {
     try {
       setLoading(true);
-      await axios.post("https://spendsense-czag.onrender.com:10000/api/users/register", values);
+      await axios.post("https://spendsense-czag.onrender.com/api/users/register", values);
       message.success("Registration Successfull");
       setLoading(false);
     } catch (error) {
